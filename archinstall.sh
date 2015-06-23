e96e4d5a8ea2f86ed5c47497b9268856f67d4964
Sean Nelson <ibanez195@gmail.com>
2015-06-22 23:58:56 -0400

    made partition formatting interactive

--- archinstall.sh
+++ archinstall.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# TODO: figure out how to fucking generate a whiptail menu command
-
 # Configure disk partitions
 whiptail --msgbox "Use cfdisk to configure your disk partitions" 10 48
 cfdisk
 
 # Obtain list of disk partitions
 parts=$(fdisk -l | grep -e "^/dev/" | awk '{print $1}')
-whiptail --msgbox "Now the partitions will be formatted" 10 40
-mkfs.ext4 /dev/sda1
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-swapon /dev/sda2
+let partcount=$(echo $parts | wc -w)
+let partcount=$partcount+1
+
+# Generate whiptail menu for partition formatting
+menucommand="whiptail --menu --noitem \"Pick a partition to format\" 15 50 $partcount"
+for x in $parts; do
+	menucommand="$menucommand \"$x\" \"\""
+done
+menucommand="$menucommand \"done\" \"\""
+
+# Ask user for formatting preferences until done is selected
+menuchoice=$(eval $menucommand 3>&1 1>&2 2>&3)
+
+while [[ $menuchoice != "done" ]]; do
+	fs=$(whiptail --menu --noitem "How would you like to format $menuchoice?" 10 50 5 "ext2" "" "ext3" "" "ext4" "" "vfat" "" "xfs" "" 3>&1 1>&2 2>&3)
+	eval "mkfs.$fs $menuchoice"
+	menuchoice=$(eval $menucommand 3>&1 1>&2 2>&3)
+done
 
 # Mount disk partitions
 mount /dev/sda3 /mnt
